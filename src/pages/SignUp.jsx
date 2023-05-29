0a649d0a9a34aa69d3f10f7e8e1cdba086b1b9d6
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-29 22:56:42 +0530

    Taostify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import {toast} from 'react-toastify'
 import { Link, Navigate } from "react-router-dom";
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import { doc, setDoc, serverTimestamp} from "firebase/firestore";
@@ -50,7 +51,7 @@ function SignUp() {
       navigate('/')
       
     } catch (error) {
-      console.log(error);
+      toast.error('Something went wrong with registration');
     }
   }
 
